0853a5021338607d417cf03d35ddf75436596034
Kirill Filatov <kirillfilatoww@mail.ru>
2024-05-21 15:13:49 +0300

    add repeat connect

--- src/main/java/ru/filatov/exchange_rates_bot/configuration/ExchangeRatesBotConfiguration.java
+++ src/main/java/ru/filatov/exchange_rates_bot/configuration/ExchangeRatesBotConfiguration.java
@@ -1,12 +1,17 @@
 package ru.filatov.exchange_rates_bot.configuration;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.filatov.exchange_rates_bot.bot.ExchangeRatesBot;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ExchangeRatesBotConfiguration {
@@ -18,11 +23,35 @@ public class ExchangeRatesBotConfiguration {
 
     }
 
+
     @Bean
     public OkHttpClient okHttpClient() {
-
-        return new OkHttpClient();
+        return new OkHttpClient.Builder()
+                .retryOnConnectionFailure(true) // Включение повторных попыток при сбое соединения
+                .addInterceptor(chain -> {
+                    Request request = chain.request();
+                    Response response = null;
+                    boolean responseOk = false;
+                    int tryCount = 0;
+                    while (tryCount < 3 && !responseOk) {
+                        try {
+                            response = chain.proceed(request);
+                            responseOk = response.isSuccessful();
+                        } catch (IOException e) {
+                            tryCount++;
+                            if (tryCount >= 3) {
+                                throw e;
+                            }
+                        }
+                    }
+                    return response;
+                })
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build();
     }
+
 
 
 
